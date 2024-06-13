e25a69ad78742aea4dbd6b208a6eb2188300ba50
Balla Kavya <ballakavya594@gmail.com>
2024-06-13 12:56:29 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
+// src/App.js
 import React from 'react';
 import './App.css';
 import Sidebar from './components/Sidebar';
 
 function App() {
-  return (
-    <div className="App">
-      <Sidebar />
-      <div className="content">
-        <h1>Welcome to My App</h1>
-        <p>Content goes here.</p>
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Sidebar />
+            <div className="content">
+                <h1>Welcome to My Website</h1>
+                <p>Click on the menu icon to open the sidebar.</p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
