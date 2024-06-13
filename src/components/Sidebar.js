e571aeaabad058d40b014cba01d81ec7af601674
Balla Kavya <ballakavya594@gmail.com>
2024-06-13 15:37:44 +0530

    Update

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import './Sidebar.css';
 import { FiHome, FiLayers, FiBook, FiPhone, FiSettings, FiUser } from 'react-icons/fi';
-import { AiFillFire,AiOutlineMoon,AiOutlineSun   } from 'react-icons/ai';
+import { AiFillFire, AiOutlineMoon, AiOutlineSun } from 'react-icons/ai';
 
 const Sidebar = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -17,7 +17,6 @@ const Sidebar = () => {
         setIsDarkMode(newMode);
         document.documentElement.classList.toggle('dark', newMode);
     };
-    
 
     return (
         <div className={`sidebar ${isOpen ? 'open' : ''}`}>
@@ -30,7 +29,7 @@ const Sidebar = () => {
             <div className="brand-name">
                 <AiFillFire />Kavya Services
             </div>
-            <nav className="nav-menu">
+            <nav className={`nav-menu ${isOpen ? 'open' : ''}`}>
                 <a href="#home"><FiHome /> Home</a>
                 <a href="#about"><FiUser /> About</a>
                 <a href="#services"><FiSettings /> Services</a>
@@ -38,7 +37,7 @@ const Sidebar = () => {
                 <a href='#blog'><FiBook /> Blog</a>
                 <a href="#contact"><FiPhone /> Contact</a>
             </nav>
-            <div className="profile-section">
+            <div className={`profile-section ${isOpen ? 'open' : ''}`}>
                 <div className="profile-picture">
                     <img src={process.env.PUBLIC_URL + '/Kavya.JPG'} alt="Profile" />
                 </div>
@@ -47,8 +46,6 @@ const Sidebar = () => {
                     <p>Web Developer</p>
                 </div>
             </div>
-          
-        
         </div>
     );
 };
